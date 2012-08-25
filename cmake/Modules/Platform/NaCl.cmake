482a9489abe5fbefa1e2babc6ec00f48a5993b72
psi <psi@7io.org>
2012-08-25 21:04:21 +0900

    NACL_SDK_ROOT͕Ê

--- cmake/Modules/Platform/NaCl.cmake
+++ cmake/Modules/Platform/NaCl.cmake
@@ -21,7 +21,6 @@ set(NACL_HOST)
 set(NACL_TARGET x86_64)
 #set(NACL_LIB glibc)
 set(NACL_LIB newlib)
-set(NACL_PEPPER pepper_21)
 
 if (APPLE)
 	set(NACL_HOST mac_x86)
@@ -33,10 +32,9 @@ endif()
 
 #すでに知られているプラットフォームじゃないとエラーが出ちゃうのでLinuxに偽装
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_VERSION ${NACL_PEPPER})
+set(CMAKE_SYSTEM_VERSION 1)
 
-set(NACL_PEPPER_PATH ${NACL_PATH}/${NACL_PEPPER})
-set(NACL_TOOLCHAIN_PATH ${NACL_PEPPER_PATH}/toolchain/${NACL_HOST}_${NACL_LIB})
+set(NACL_TOOLCHAIN_PATH ${NACL_PATH}/toolchain/${NACL_HOST}_${NACL_LIB})
 
 set(CMAKE_FIND_ROOT_PATH
 	${NACL_TOOLCHAIN_PATH}
@@ -48,7 +46,7 @@ set(CMAKE_C_COMPILER   ${NACL_TOOLCHAIN_PATH}/bin/${NACL_TARGET}-nacl-gcc)
 set(CMAKE_CXX_COMPILER ${NACL_TOOLCHAIN_PATH}/bin/${NACL_TARGET}-nacl-g++)
 set(CMAKE_LINKER ${NACL_TOOLCHAIN_PATH}/bin/${NACL_TARGET}-nacl-g++)
 set(CMAKE_OBJDUMP ${NACL_TOOLCHAIN_PATH}/${NACL_TARGET}-nacl/bin/objdump)
-set(CMAKE_NMF python ${NACL_PEPPER_PATH}/tools/create_nmf.py)
+set(CMAKE_NMF python ${NACL_PATH}/tools/create_nmf.py)
 
 set(CMAKE_C_FLAGS 
 	CACHE STRING "OpenEmbedded - GCC/C flags" FORCE
